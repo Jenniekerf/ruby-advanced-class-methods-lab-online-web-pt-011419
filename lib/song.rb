83eac632e160d1ef2d45f571b67e9258812425bf
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-03-08 20:37:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,74 +1,82 @@
+require 'pry'
 
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-  
-  def self.create
-    song = self.new 
-    @@all << song
-    song
+ 
+ def self.create
+   song = self.new
+   song.save
+   song
+ end
+ 
+ def self.new_by_name(name)
+   song = self.new 
+   song.name = name
+   song
+ end
+ 
+ def self.create_by_name(name)
+   song = self.new
+   song.name = name
+   song.save
+   song
+ end
+ 
+ def self.find_by_name(name)
+   @@all.find {|title| title.name == name}
+   end
+   
+ def self.find_or_create_by_name(name)
+     self.find_by_name(name) || self.create_by_name(name) 
   end
   
-  def self.new_by_name(name)
-    song = self.new
-    song.name = name
-    song
-  end
-  
-  
-  def self.create_by_name(name)
-    song = self.new
-    song.name = name
-    @@all << song
-    song
-  end
-  
-  def self.find_by_name(name)
-    @@all.find { |song| song.name == name } 
-  end
-  
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
-    end
-    
   def self.alphabetical
-    @@all.sort_by { |s| s.name }
+    @@all.sort_by {|song| song.name}
   end
   
-  def self.new_from_filename(file_name)
-    parsed = file_name.split(" - ")
+  def self.new_from_filename(song_name)
+    parsed = song_name.split(" - ")
     artist_name = parsed[0]
-    song_name = parsed[1].split(".").reject {|e| e == "mp3"}.join
+    song_name = parsed[1].chomp(".mp3")
     
     song = self.new
-    song.name = song_name
-    song.artist_name = artist_name 
+    song.name = song_name 
+    song.artist_name = artist_name
     song
-    end
-    
-  def self.create_from_filename(file_name)
-    parsed = file_name.split(" - ")
+  end
+  
+  def self.create_from_filename(song_name)
+    parsed = song_name.split(" - ")
     artist_name = parsed[0]
-    song_name = parsed[1].split(".").reject {|e| e == "mp3"}.join
+    song_name = parsed[1].chomp(".mp3")
     
-    song = self.create
-    song.name = song_name
-    song.artist_name = artist_name 
-    end
-    
-    def self.destroy_all
-      @@all.clear
-    end
+    song = self.new
+    song.name = song_name 
+    song.artist_name = artist_name
+    song.save
+  end
+  
+  def self.destroy_all 
+    @@all.clear
+  end
+  
+  
+
+     
+     
 
 
+ 
+ 
 end
 
 
